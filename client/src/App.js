14ca8d99ea18acf0ed300b1756609636265b3e5b
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-04-08 23:34:06 +0530

    feat : schedule page & calendar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Schedule from "./pages/Schedule";
 import Enroll from "./pages/Enroll";
 
 function App() {
   return (
     <>
       <Header />
-      <Enroll />
+      <Schedule />
       <Footer />
     </>
   );
--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -1,7 +1,21 @@
 import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction";
 
 const Schedule = () => {
-  return <div>Schedule</div>;
+  const handleDateClick = (arg) => {
+    console.log(arg.dateStr);
+    alert(arg.dateStr);
+  };
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      weekends={true}
+      dateClick={handleDateClick}
+    />
+  );
 };
 
 export default Schedule;
