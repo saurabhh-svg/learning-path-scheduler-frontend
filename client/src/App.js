444605d6f281a0cc81036f254f7909c8272a8746
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-04-08 20:08:04 +0530

    feat : Tailwind CSS setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div className="text-rose-500 text-2xl">Header</div>;
+};
+
+export default Header;
