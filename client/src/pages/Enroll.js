81a59133b4ed39be3822e96be9720455c81f05b4
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-04-08 22:50:33 +0530

    feat : enroll page api call

--- client/src/pages/Enroll.js
+++ client/src/pages/Enroll.js
@@ -1,6 +1,37 @@
 import React from "react";
+import moment from "moment";
+import axios from "axios";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
-const enroll = () => {
+const Enroll = () => {
+  const [course, setCourse] = useState("java");
+  const [hours, setHours] = useState(2);
+  const [date] = useState(moment().format("YYYY-MM-DD"));
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setIsFormSubmitting(true);
+
+    const choice = new FormData();
+    choice.append("course", course);
+    choice.append("hours", hours);
+    choice.append("date", date);
+
+    axios
+      .post("/api/enroll/", choice)
+      .then((res) => {
+        setIsFormSubmitting(false);
+        alert("Enrolled Successfully!");
+        Navigate("/schedule");
+      })
+      .catch((error) => {
+        setIsFormSubmitting(false);
+        alert(error.response.data.message);
+      });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center ">
       <label
@@ -12,12 +43,10 @@ const enroll = () => {
       <select
         id="courses"
         class="bg-gray-50 cursor-pointer border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:hover:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={(e) => setCourse(e.target.value)}
       >
         <option selected>Choose a Course</option>
         <option value="JAVA">JAVA</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
       </select>
       <label
         for="hours you can devote"
@@ -28,21 +57,23 @@ const enroll = () => {
       <select
         id="courses"
         class="bg-gray-50  cursor-pointer border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-900 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={(e) => setHours(e.target.value)}
       >
         <option selected>Hours you can devote per day</option>
-        <option value="two">Two</option>
-        <option value="four">Four</option>
-        <option value="six">Six</option>
-        <option value="eight">Eight</option>
+        <option value="2">Two</option>
+        <option value="4">Four</option>
+        <option value="6">Six</option>
       </select>
       <button
         type="button"
+        onClick={handleFormSubmit}
+        disabled={isFormSubmitting}
         class="text-white mt-5 bg-gray-800 focus:outline-none focus:ring-4  focus:ring-gray-300 font-medium rounded-lg w-32 text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-700 dark:hover:bg-gray-900 dark:focus:ring-gray-700 dark:border-gray-600"
       >
-        Submit
+        {isFormSubmitting ? "Submitting..." : "Submit"}
       </button>
     </div>
   );
 };
 
-export default enroll;
+export default Enroll;
