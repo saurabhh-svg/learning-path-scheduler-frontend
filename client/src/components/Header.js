db93dc235eadad1ba3b866b3af44da3072cf2426
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-04-09 23:46:22 +0530

    fix : enroll route update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <a
-                  href="/enroll"
+                  href="/"
                   className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                   aria-current="page"
                 >
