2e54556282b643e29db551e4dd108c4fc28a45b2
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-04-09 17:50:19 +0530

    feat : Header reformat

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -29,7 +29,7 @@ export default function Header() {
                   class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                   aria-current="page"
                 >
-                  Enroll
+                  Click to Enroll
                 </a>
               </li>
             </ul>
